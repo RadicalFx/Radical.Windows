f1575f751f2a2f14f0fa836297dffe13b224de20
Nazareno Manco <nazareno.m@live.com>
2017-01-21 14:28:58 +0200

    Corrected EnumBooleanConverter implementation (#48)

--- src/net35/Radical.Windows/Presentation/Converters/EnumBooleanConverter.cs
+++ src/net35/Radical.Windows/Presentation/Converters/EnumBooleanConverter.cs
@@ -11,6 +11,34 @@ namespace Topics.Radical.Windows.Presentation.Converters
     /// </summary>
     public class EnumBooleanConverter : IValueConverter
     {
+
+        /// <summary>
+        /// Check to see if a flags enumeration has a specific flag set.
+        /// </summary>
+        /// <param name="variable">Flags enumeration to check</param>
+        /// <param name="value">Flag to check for</param>
+        /// <returns></returns>
+        public bool HasFlag(Enum variable, Enum value)
+        {
+            if (variable == null)
+                return false;
+
+            if (value == null)
+                throw new ArgumentNullException("value");
+
+            // Not as good as the .NET 4 version of this function, but should be good enough
+            if (!Enum.IsDefined(variable.GetType(), value))
+            {
+                throw new ArgumentException(string.Format(
+                    "Enumeration type mismatch.  The flag is of type '{0}', was expecting '{1}'.",
+                    value.GetType(), variable.GetType()));
+            }
+
+            ulong num = System.Convert.ToUInt64(value);
+            return ((System.Convert.ToUInt64(variable) & num) == num);
+
+        }
+
         #region IValueConverter Members
         /// <summary>
         /// Modifies the source data before passing it to the target to display in the UI.
@@ -24,16 +52,7 @@ namespace Topics.Radical.Windows.Presentation.Converters
         /// </returns>
         public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            string parameterString = parameter as string;
-            if (parameterString == null)
-                return DependencyProperty.UnsetValue;
-
-            if (Enum.IsDefined(value.GetType(), value) == false)
-                return DependencyProperty.UnsetValue;
-
-            object parameterValue = Enum.Parse(value.GetType(), parameterString);
-
-            return parameterValue.Equals(value);
+            return HasFlag((Enum)value, (Enum)parameter);
         }
 
         /// <summary>
@@ -48,11 +67,7 @@ namespace Topics.Radical.Windows.Presentation.Converters
         /// </returns>
         public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            string parameterString = parameter as string;
-            if (parameterString == null)
-                return DependencyProperty.UnsetValue;
-
-            return Enum.Parse(targetType, parameterString);
+            return value.Equals(true) ? parameter : Binding.DoNothing;
         }
         #endregion
     }
