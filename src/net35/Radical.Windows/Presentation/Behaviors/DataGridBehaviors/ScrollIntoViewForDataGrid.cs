c674a683fc41ddf5b06f1f5407c084555fdccfb8
Nazareno Manco <nazareno.m@live.com>
2017-02-24 18:42:22 +0100

    Added ScrollIntoView for DataGrid Control

---
+++ src/net35/Radical.Windows/Presentation/Behaviors/DataGridBehaviors/ScrollIntoViewForDataGrid.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Topics.Radical.Windows.Presentation.Behaviors.DataGridBehaviors
+{
+    class ScrollIntoViewForListBox
+    {
+    }
+}
---
+++ src/net40/Radical.Windows/Presentation/Behaviors/DataGridBehaviors/ScrollIntoViewForDataGrid.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using Topics.Radical.Windows.Behaviors;
+
+namespace Topics.Radical.Windows.Presentation.Behaviors
+{
+    class ScrollIntoViewForDataGrid : RadicalBehavior<DataGrid>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.SelectionChanged += AssociatedObject_SelectionChanged;
+        }
+
+        void AssociatedObject_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (sender is DataGrid)
+            {
+                DataGrid dataGrid = (sender as DataGrid);
+                if (dataGrid.SelectedItem != null)
+                {
+                    dataGrid.Dispatcher.BeginInvoke(
+                        (Action)(() =>
+                        {
+                            dataGrid.UpdateLayout();
+                            if (dataGrid.SelectedItem !=
+                                null)
+                                dataGrid.ScrollIntoView(
+                                    dataGrid.SelectedItem);
+                        }));
+                }
+            }
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.SelectionChanged -=
+                AssociatedObject_SelectionChanged;
+
+        }
+    }
+}
