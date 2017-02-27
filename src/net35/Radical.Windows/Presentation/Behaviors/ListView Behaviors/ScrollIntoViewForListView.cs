90fecf1fe6e6c0e86c8f28b690f006bc65391981
Nazareno Manco <nazareno.m@live.com>
2017-02-27 12:05:43 +0100

    Added ScrollIntoView for ListView Control (#62)

---
+++ src/net35/Radical.Windows/Presentation/Behaviors/ListView Behaviors/ScrollIntoViewForListView.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Controls;
+using Topics.Radical.Conversions;
+
+namespace Topics.Radical.Windows.Behaviors
+{
+    public class ScrollIntoViewForListView : RadicalBehavior<ListView>
+    {
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            this.AssociatedObject.SelectionChanged += OnAssociatedObjectSelectionChanged;
+        }
+
+        void OnAssociatedObjectSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            sender.As<ListView>(listView =>
+            {
+                if (listView.SelectedItem != null)
+                {
+                    listView.Dispatcher.BeginInvoke(
+                        (Action)(() =>
+                        {
+                            listView.UpdateLayout();
+                            if (listView.SelectedItem != null)
+                                listView.ScrollIntoView(listView.SelectedItem);
+                        }));
+                }
+            });
+
+        }
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            this.AssociatedObject.SelectionChanged -= OnAssociatedObjectSelectionChanged;
+
+        }
+    }
+}
