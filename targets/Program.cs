82bd3f14bfc1b8df76fd85fdda37d5ea98784a60
Mauro Servienti <mauro.servienti@gmail.com>
2019-07-06 17:56:03 +0200

    MSBuild compatible argument

--- targets/Program.cs
+++ targets/Program.cs
@@ -12,7 +12,7 @@ internal class Program
 
         Target("build",
             Directory.EnumerateFiles("src", "*.sln", SearchOption.AllDirectories),
-            solution => Run(sdk.GetDotnetCliPath(), $"msbuild \"{solution}\" --configuration Release"));
+            solution => Run(sdk.GetDotnetCliPath(), $"msbuild \"{solution}\" -p:Configuration=Release"));
 
         Target("test", DependsOn("build"),
             Directory.EnumerateFiles("src", "*Tests.csproj", SearchOption.AllDirectories),
