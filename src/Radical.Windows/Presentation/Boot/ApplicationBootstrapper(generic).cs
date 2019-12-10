597caa4749ab5c21a02ce50b999b8d21b4dfe594
mauroservienti <mauro.servienti@gmail.com>
2019-12-10 11:31:49 +0100

    Apply generic constraints to TMainView

--- src/Radical.Windows/Presentation/Boot/ApplicationBootstrapper(generic).cs
+++ src/Radical.Windows/Presentation/Boot/ApplicationBootstrapper(generic).cs
@@ -2,18 +2,18 @@
 
 namespace Radical.Windows.Presentation.Boot
 {
-    public class ApplicationBootstrapper<TMainView> : ApplicationBootstrapper
+    public class ApplicationBootstrapper<TMainView> : ApplicationBootstrapper where TMainView : System.Windows.Window
     {
         public ApplicationBootstrapper()
             : base()
         {
-
+            UsingAsShell<TMainView>();
         }
 
         public ApplicationBootstrapper(IServiceCollection services)
             : base(services)
         {
-
+            UsingAsShell<TMainView>();
         }
     }
 }
