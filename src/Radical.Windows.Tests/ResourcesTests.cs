0dae674c51c5394ba0ed2f44fbb4ba1bdebb5455
Mauro Servienti <mauro.servienti@gmail.com>
2019-12-30 20:14:07 +0100

    Embed as resource, add test

---
+++ src/Radical.Windows.Tests/ResourcesTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Radical.Windows.Tests
+{
+    [TestClass()]
+    public class ResourcesTests
+    {
+        [TestMethod]
+        public void GrayscaleEffect_ctor_should_load_resource() 
+        {
+            if (!UriParser.IsKnownScheme("pack"))
+                new System.Windows.Application();
+
+            new Effects.GrayscaleEffect();
+        }
+    }
+}
