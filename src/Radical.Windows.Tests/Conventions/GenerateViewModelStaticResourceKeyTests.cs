2de57ad9b025a5b98a9ccbdbd8bcddb8c7920766
Mauro Servienti <mauro.servienti@gmail.com>
2020-01-20 16:27:08 +0100

    add tests

---
+++ src/Radical.Windows.Tests/Conventions/GenerateViewModelStaticResourceKeyTests.cs
@@ -0,0 +1,38 @@
+﻿using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Radical.ComponentModel.Messaging;
+using Radical.Windows.Presentation.Boot;
+using Radical.Windows.Presentation.ComponentModel;
+using Radical.Windows.Presentation.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Radical.Windows.Tests.Conventions
+{
+    [TestClass]
+    public class GenerateViewModelStaticResourceKeyTests
+    {
+        [TestMethod, TestCategory("Conventions")]
+        public void GenerateViewModelStaticResourceKey_should_generate_expected_key()
+        {
+            var obj = new GenericParameterHelper();
+            var expected = obj.GetType().Name;
+            var conventions = new ConventionsHandler(A.Fake<IMessageBroker>(), A.Fake<IReleaseComponents>(), new BootstrapConventions());
+            var key = conventions.GenerateViewModelStaticResourceKey(obj);
+
+            Assert.AreEqual(expected, key);
+        }
+
+        [TestMethod, TestCategory("Conventions")]
+        public void DefaultGenerateViewModelStaticResourceKey_should_generate_expected_key() 
+        {
+            var obj = new GenericParameterHelper();
+            var expected = obj.GetType().Name;
+            var conventions = new ConventionsHandler(A.Fake<IMessageBroker>(), A.Fake<IReleaseComponents>(), new BootstrapConventions());
+            var key = conventions.DefaultGenerateViewModelStaticResourceKey(obj);
+
+            Assert.AreEqual(expected, key);
+        }
+    }
+}
