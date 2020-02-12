c6e303867cb492f9e7ea2dc94dfbceb8ecf94649
mauroservienti <mauro.servienti@gmail.com>
2020-02-12 15:45:35 +0100

    Add a test to verify ViewResolver GetView

--- src/Radical.Windows.Tests/Presentation/Regions/Specialized/TacControlRegionTests.cs
+++ src/Radical.Windows.Tests/Presentation/Regions/Specialized/TacControlRegionTests.cs
@@ -2,6 +2,7 @@
 using Radical.Windows.ComponentModel;
 using Radical.Windows.Presentation;
 using Radical.Windows.Regions;
+using Radical.Windows.Tests;
 using System;
 using System.Threading;
 using System.Windows;
@@ -106,50 +107,6 @@ namespace Test.Radical.Windows.Presentation.Regions.Specialized
                 Assert.IsTrue(vm1.Invoked);
                 Assert.IsTrue(vm2.Invoked);
             });
-        }
-    }
-
-    class TestRunner
-    {
-        readonly Action test;
-        readonly ApartmentState state;
-        Exception ex;
-
-        private TestRunner(ApartmentState state, Action test)
-        {
-            this.state = state;
-            this.test = test;
-        }
-
-        public static void Execute(ApartmentState state, Action test)
-        {
-            var runner = new TestRunner(state, test);
-            runner.Execute();
-        }
-
-        private void Execute()
-        {
-            var worker = new Thread(() =>
-            {
-                try
-                {
-                    this.test();
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    this.ex = e;
-                }
-            });
-
-            worker.SetApartmentState(this.state);
-            worker.Start();
-            worker.Join();
-
-            if (this.ex != null)
-            {
-                throw this.ex;
-            }
         }
     }
 }
---
+++ src/Radical.Windows.Tests/Services/ViewResolverTests.cs
@@ -0,0 +1,32 @@
+﻿using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Radical.ComponentModel.Messaging;
+using Radical.Windows.Bootstrap;
+using Radical.Windows.ComponentModel;
+using Radical.Windows.Services;
+using Radical.Windows.Tests.Services.Presentation;
+using System;
+using System.Threading;
+
+namespace Radical.Windows.Tests.Services
+{
+    [TestClass()]
+    public class ViewResolverTests
+    {
+        [TestMethod()]
+        public void ViewResolver_resolve_should_resolve_expected_view() 
+        {
+            TestRunner.Execute(ApartmentState.STA, () =>
+            {
+                var conventions = new ConventionsHandler(A.Fake<IMessageBroker>(), A.Fake<IReleaseComponents>(), new BootstrapConventions());
+                var container = A.Fake<IServiceProvider>();
+                A.CallTo(() => container.GetService(typeof(AnEmptyView))).Returns(new AnEmptyView());
+                var sut = new ViewResolver(container, conventions, new ResourcesRegistrationHolder());
+
+                var view = sut.GetView<AnEmptyView>();
+
+                Assert.IsNotNull(view);
+            });
+        }
+    }
+}
