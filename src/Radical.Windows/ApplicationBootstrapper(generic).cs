a12e84483153840d8d706ece79d3fcff870a5ae8
mauroservienti <mauro.servienti@gmail.com>
2020-02-17 16:15:16 +0100

    Obsolete ApplicationBootstrapper

--- src/Radical.Windows/ApplicationBootstrapper(generic).cs
+++ src/Radical.Windows/ApplicationBootstrapper(generic).cs
@@ -1,5 +1,8 @@
-﻿namespace Radical.Windows
+﻿using System;
+
+namespace Radical.Windows
 {
+    [Obsolete("ApplicationBootstrapper has been obsoleted and will be removed in v3.0.0, consider moving to new RadicalApplication using the AddRadicalApplication extension method.", false)]
     public class ApplicationBootstrapper<TMainView> : ApplicationBootstrapper where TMainView : System.Windows.Window
     {
         public ApplicationBootstrapper()
