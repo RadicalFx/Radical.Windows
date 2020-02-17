199407ed13ed1bdb30f92016b905bbc617d76f2c
Mauro Servienti <mauro.servienti@gmail.com>
2020-02-17 18:19:39 +0100

    smoke test applications

---
+++ src/RadicalApp/Presentation/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace RadicalApp.Presentation
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/RadicalAppWithGenericHost/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using System.Windows;
+
+namespace RadicalAppWithGenericHost
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        public App()
+        {
+            var host = new HostBuilder()
+                .AddRadicalApplication<Presentation.MainView>()
+                .Build();
+
+            Startup += async (s, e) =>
+            {
+                await host.StartAsync();
+            };
+
+            Exit += async (s, e) =>
+            {
+                using (host)
+                {
+                    await host?.StopAsync();
+                }
+            };
+        }
+    }
+}
