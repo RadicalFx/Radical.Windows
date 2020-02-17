1529c3be62fb383c2466db710add06f6d8a33578
mauroservienti <mauro.servienti@gmail.com>
2020-02-17 16:03:31 +0100

    Application extension to bind the new Radical Application and tests

---
+++ src/Radical.Windows.Tests/Boot/RadicalApplicationTests.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Radical.Windows.ComponentModel;
+using System;
+using System.Windows;
+
+namespace Radical.Windows.Tests.Boot
+{
+    [TestClass]
+    public class RadicalApplicationTests
+    {
+        [SharedApplicationTestMethod]
+        public void ApplicationBoot_process_should_create_valid_container()
+        {
+            var bootCompleted = false;
+            IServiceProvider container = null;
+
+            var radicalApplication = Application.Current.AddRadicalApplication(configuration =>
+            {
+                configuration.DisableAutoBoot();
+                configuration.OnServiceProviderCreated(serviceProvider => container = serviceProvider);
+                configuration.OnBootCompleted(_ => bootCompleted = true);
+            });
+
+            radicalApplication.Boot();
+
+            var viewResolver = container.GetService<IViewResolver>();
+
+            Assert.IsNotNull(viewResolver);
+            Assert.IsTrue(bootCompleted);
+        }
+    }
+}
---
+++ src/Radical.Windows/System.Windows.Application.Extensions.cs
@@ -0,0 +1,47 @@
+﻿using Radical.Windows;
+using Radical.Windows.Bootstrap;
+
+namespace System.Windows
+{
+    public static class ApplicationExtensions
+    {
+        /// <summary>
+        /// Add a RadicalApplication to the current WPF application
+        /// </summary>
+        public static RadicalApplication AddRadicalApplication(this Application application, Action<BootstrapConfiguration> configure)
+        {
+            return AddRadicalApplication(application, null, configure);
+        }
+
+        /// <summary>
+        /// Add a RadicalApplication to the current WPF application
+        /// </summary>
+        public static RadicalApplication AddRadicalApplication<TShellView>(this Application application) where TShellView : Window
+        {
+            return AddRadicalApplication(application, typeof(TShellView), null);
+        }
+
+        /// <summary>
+        /// Add a RadicalApplication to the current WPF application
+        /// </summary>
+        public static RadicalApplication AddRadicalApplication<TShellView>(this Application application, Action<BootstrapConfiguration> configure) where TShellView : Window
+        {
+            return AddRadicalApplication(application, typeof(TShellView), configure);
+        }
+
+        /// <summary>
+        /// Add a RadicalApplication to the current WPF application
+        /// </summary>
+        public static RadicalApplication AddRadicalApplication(this Application application, Type shellViewType, Action<BootstrapConfiguration> configure)
+        {
+            var configuration = new BootstrapConfiguration();
+            if (shellViewType != null)
+            {
+                configuration.UseAsShell(shellViewType);
+            }
+            configure?.Invoke(configuration);
+
+            return RadicalApplication.BoundTo(application, configuration);
+        }
+    }
+}
