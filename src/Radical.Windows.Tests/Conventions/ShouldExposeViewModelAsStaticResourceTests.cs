e7b3c2d542dcf6e86b488b6b2a7d2a2584d5219b
Mauro Servienti <mauro.servienti@gmail.com>
2020-09-26 13:41:03 +0200

    Support exposing ViewModels as static resources using an attribute (#311)
    * Support exposing ViewModels as static resources using an attribute
    
    * tests
    
    * public API

---
+++ src/Radical.Windows.Tests/Conventions/ShouldExposeViewModelAsStaticResourceTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+using FakeItEasy;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Radical.ComponentModel.Messaging;
+using Radical.Windows.Bootstrap;
+using Radical.Windows.ComponentModel;
+using Radical.Windows.Services;
+
+namespace Radical.Windows.Tests.Conventions
+{
+    [TestClass]
+    public class ShouldExposeViewModelAsStaticResourceTests
+    {
+        class ViewModelNoAttribute
+        {
+        }
+
+        [ExposeViewModelAsStaticResource]
+        class ViewModelWithAttribute
+        {
+        }
+
+        class NopView : DependencyObject
+        {
+        }
+
+        [TestMethod, TestCategory("Conventions")]
+        public void ShouldExposeViewModelAsStaticResource_should_return_false_if_no_attribute()
+        {
+            var conventions = new ConventionsHandler(A.Fake<IMessageBroker>(), A.Fake<IReleaseComponents>(), new BootstrapConventions());
+            var key = conventions.ShouldExposeViewModelAsStaticResource(new NopView(), new ViewModelNoAttribute());
+
+            Assert.AreEqual(false, key);
+        }
+
+        [TestMethod, TestCategory("Conventions")]
+        public void ShouldExposeViewModelAsStaticResource_should_return_false_if_attribute()
+        {
+            var conventions = new ConventionsHandler(A.Fake<IMessageBroker>(), A.Fake<IReleaseComponents>(), new BootstrapConventions());
+            var key = conventions.ShouldExposeViewModelAsStaticResource(new NopView(), new ViewModelWithAttribute());
+
+            Assert.AreEqual(true, key);
+        }
+    }
+}
---
+++ src/Radical.Windows/ComponentModel/ExposeViewModelAsStaticResourceAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Radical.Windows.ComponentModel
+{
+    /// <summary>
+    /// Applied to a ViewModel to expose it as a static resource in the bound View.
+    /// </summary>
+    [AttributeUsage( AttributeTargets.Class, Inherited = false )]
+    public class ExposeViewModelAsStaticResourceAttribute : Attribute
+    {
+    }
+}
