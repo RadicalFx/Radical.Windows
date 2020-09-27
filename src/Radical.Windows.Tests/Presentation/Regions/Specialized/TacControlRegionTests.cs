f23aca22de2e16a3d7682226522fc7585dee0cb3
Mauro Servienti <mauro.servienti@gmail.com>
2020-09-27 06:57:34 +0200

    tests

--- src/Radical.Windows.Tests/Presentation/Regions/Specialized/TacControlRegionTests.cs
+++ src/Radical.Windows.Tests/Presentation/Regions/Specialized/TacControlRegionTests.cs
@@ -103,5 +103,78 @@ namespace Test.Radical.Windows.Presentation.Regions.Specialized
             Assert.IsTrue(vm1.Invoked);
             Assert.IsTrue(vm2.Invoked);
         }
+        
+        [SingleThreadedApartmentTestMethod, TestCategory( "TabControlRegion" ), TestCategory( "Regions" ), TestCategory( "UI Composition" )]
+        public void TabControlRegion_when_PreserveViewDataContext_true_should_assign_view_ViewModel_to_header_data_context()
+        {
+            var sut = new TestTabControlRegion();
+            sut.ProvideValue(new HardCodedServiceProvider());
+
+            var vm = new TestViewModel();
+            var tabView = new UserControl()
+            {
+                DataContext = vm
+            };
+            
+            var headerView = new TextBlock();
+            
+            RegionHeaderedElement.SetHeader(tabView, headerView);
+            RegionHeaderedElement.SetPreserveOwningRegionDataContext(headerView, true);
+            
+            sut.Add(tabView);
+            
+            Assert.AreEqual(vm, headerView.DataContext);
+        }
+        
+        [SingleThreadedApartmentTestMethod, TestCategory( "TabControlRegion" ), TestCategory( "Regions" ), TestCategory( "UI Composition" )]
+        public void TabControlRegion_when_PreserveViewDataContext_false_should_not_set_header_data_context()
+        {
+            var sut = new TestTabControlRegion();
+            sut.ProvideValue(new HardCodedServiceProvider());
+
+            var vm = new TestViewModel();
+            var tabView = new UserControl()
+            {
+                DataContext = vm
+            };
+            
+            var headerView = new TextBlock()
+            {
+                DataContext = new TestViewModel()
+            };
+            
+            RegionHeaderedElement.SetHeader(tabView, headerView);
+            RegionHeaderedElement.SetPreserveOwningRegionDataContext(headerView, false);
+            
+            sut.Add(tabView);
+            
+            Assert.AreNotEqual(vm, headerView.DataContext);
+        }
+        
+        [SingleThreadedApartmentTestMethod, TestCategory( "TabControlRegion" ), TestCategory( "Regions" ), TestCategory( "UI Composition" )]
+        public void TabControlRegion_PreserveViewDataContext_defaults_to_false_and_should_not_set_header_data_context()
+        {
+            var sut = new TestTabControlRegion();
+            sut.ProvideValue(new HardCodedServiceProvider());
+
+            var vm = new TestViewModel();
+            var tabView = new UserControl()
+            {
+                DataContext = vm
+            };
+            
+            var headerView = new TextBlock()
+            {
+                DataContext = new TestViewModel()
+            };
+            
+            RegionHeaderedElement.SetHeader(tabView, headerView);
+            //PreserveOwningRegionDataContext defaults to false
+            //RegionHeaderedElement.SetPreserveOwningRegionDataContext(headerView, false);
+            
+            sut.Add(tabView);
+            
+            Assert.AreNotEqual(vm, headerView.DataContext);
+        }
     }
 }
